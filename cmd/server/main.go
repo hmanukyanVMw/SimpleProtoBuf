a3be0d99d49dbf23fc8651e9ce8bf3161804a88f
hmanukyan <hmanukyan@vmware.com>
2023-11-19 17:15:00 +0400

    add project

---
+++ cmd/server/main.go
@@ -0,0 +1,29 @@
+// server.go
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/hmanukyanVMw/gen/go/sso"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (s *server) MyMethod(ctx context.Context, in *pb.MyRequest) (*pb.MyResponse, error) {
+	return &pb.MyResponse{Result: "Hello " + in.Data}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterMyServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
